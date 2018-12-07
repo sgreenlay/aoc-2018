259ec902d1ff1a5ea7f49cae59c008fbfa84cdeb
Scott Greenlay <sgreenlay@gmail.com>
2018-12-06 23:13:47 -0800

    Day 7 - Part 2

--- day7.cpp
+++ day7.cpp
@@ -53,50 +53,142 @@ int main()
 {
     auto steps = stepsFromFile("data/day7.txt");
 
-    std::map<char, std::set<char>> requirements;
-    for (auto && step : steps)
     {
-        if (requirements.count(step.label) == 0)
+        std::map<char, std::set<char>> requirements;
+        for (auto && step : steps)
         {
-            requirements[step.label] = std::set<char>();
-        }
-        requirements[step.next].emplace(step.label);
-    }
-
-    std::string visited;
-    std::priority_queue<char, std::vector<char>, std::greater<char>> potentials;
-
-    while (!requirements.empty())
-    {
-        std::vector<char> remove;
-        for (auto && requirement : requirements)
-        {
-            if (requirement.second.empty())
+            if (requirements.count(step.label) == 0)
             {
-                potentials.emplace(requirement.first);
-                remove.push_back(requirement.first);
+                requirements[step.label] = std::set<char>();
+            }
+            requirements[step.next].emplace(step.label);
+        }
+
+        std::string visited;
+        std::priority_queue<char, std::vector<char>, std::greater<char>> potentials;
+
+        while (!requirements.empty())
+        {
+            std::vector<char> remove;
+            for (auto && requirement : requirements)
+            {
+                if (requirement.second.empty())
+                {
+                    potentials.emplace(requirement.first);
+                    remove.push_back(requirement.first);
+                }
+            }
+
+            for (auto && c : remove)
+            {
+                requirements.erase(c);
+            }
+
+            char c = potentials.top();
+            visited.push_back(c);
+            potentials.pop();
+
+            for (auto && requirement : requirements)
+            {
+                if (requirement.second.count(c) > 0)
+                {
+                    requirement.second.erase(c);
+                }
             }
         }
 
-        for (auto && c : remove)
-        {
-            requirements.erase(c);
-        }
-
-        char c = potentials.top();
-        visited.push_back(c);
-        potentials.pop();
-
-        for (auto && requirement : requirements)
-        {
-            if (requirement.second.count(c) > 0)
-            {
-                requirement.second.erase(c);
-            }
-        }
+        printf("Part 1: %s\n", visited.c_str());
     }
 
-    printf("%s\n", visited.c_str());
+    {
+        std::map<char, std::set<char>> requirements;
+        for (auto && step : steps)
+        {
+            if (requirements.count(step.label) == 0)
+            {
+                requirements[step.label] = std::set<char>();
+            }
+            requirements[step.next].emplace(step.label);
+        }
+
+        int elf_count = 5;
+        int base_cost = 60;
+
+        struct work {
+            char ch;
+            int start;
+            int duration;
+
+            int total() const {
+                return start + duration;
+            }
+        };
+
+        std::string visited;
+        std::vector<work> elves;
+
+        int clock = 0;
+        while (!requirements.empty())
+        {
+            std::vector<char> remove;
+            for (auto && requirement : requirements)
+            {
+                if (requirement.second.empty())
+                {
+                    int cost = requirement.first - 'A' + 1 + base_cost;
+
+                    elves.push_back(work{ requirement.first, clock, cost });
+
+                    remove.push_back(requirement.first);
+
+                    //printf("%d : Elf started %c with cost %d\n", clock, requirement.first, cost);
+
+                    if (elves.size() == elf_count)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (auto && c : remove)
+            {
+                requirements.erase(c);
+            }
+
+            std::sort(elves.begin(), elves.end(), [](auto && a, auto && b) {
+                return a.total() < b.total();
+                });
+
+            auto elf = elves[0];
+            clock += elf.total() - clock;
+
+            while (!elves.empty())
+            {
+                elf = elves[0];
+
+                if (elf.total() > clock)
+                {
+                    break;
+                }
+
+                elves.erase(elves.begin());
+
+                //printf("%d : Elf completed %c\n", clock, elf);
+                visited.push_back(elf.ch);
+
+                for (auto && requirement : requirements)
+                {
+                    if (requirement.second.count(elf.ch) > 0)
+                    {
+                        requirement.second.erase(elf.ch);
+                    }
+                }
+            }
+        }
+
+        printf("Part 2: %s\n", visited.c_str());
+        printf("  Takes %d elves %d seconds\n", elf_count, clock);
+    }
 
     std::getc(stdin);
 }
