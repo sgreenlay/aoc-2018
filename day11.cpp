0a47fce3a1b99c18010a757f84bc4081bd0ef6d8
Scott Greenlay <sgreenlay@gmail.com>
2018-12-10 21:50:45 -0800

    Day 11 - Part 2

--- day11.cpp
+++ day11.cpp
@@ -27,6 +27,18 @@ int powerLevel(int x, int y, int sn)
     return power;
 }
 
+int min(int a, int b)
+{
+    if (a < b) return a;
+    return b;
+}
+
+int max(int a, int b)
+{
+    if (a > b) return a;
+    return b;
+}
+
 int main()
 {
     int sn = 9810;
@@ -43,35 +55,78 @@ int main()
         }
     }
 
-    int xMax = 0;
-    int yMax = 0;
-    int max = 0;
-
-    for (int y = 1; y < 299; ++y)
+    // Part 1
     {
-        for (int x = 1; x < 299; ++x)
-        {
-            int sum =
-                cells[y - 1][x - 1] +
-                cells[y - 1][x] +
-                cells[y - 1][x + 1] +
-                cells[y][x - 1] +
-                cells[y][x] +
-                cells[y][x + 1] +
-                cells[y + 1][x - 1] +
-                cells[y + 1][x] +
-                cells[y + 1][x + 1];
+        int xMax = 0;
+        int yMax = 0;
+        int max = 0;
 
-            if (sum > max)
+        for (int y = 1; y < 299; ++y)
+        {
+            for (int x = 1; x < 299; ++x)
             {
-                xMax = x;
-                yMax = y;
-                max = sum;
+                int sum =
+                    cells[y - 1][x - 1] +
+                    cells[y - 1][x] +
+                    cells[y - 1][x + 1] +
+                    cells[y][x - 1] +
+                    cells[y][x] +
+                    cells[y][x + 1] +
+                    cells[y + 1][x - 1] +
+                    cells[y + 1][x] +
+                    cells[y + 1][x + 1];
+
+                if (sum > max)
+                {
+                    xMax = x;
+                    yMax = y;
+                    max = sum;
+                }
             }
         }
+
+        printf("Max power of %d starting at (%d, %d)\n", max, xMax, yMax);
     }
 
-    printf("Max power of %d starting at (%d, %d)\n", max, xMax, yMax);
+    // Part 2
+    {
+        int xMax = 0;
+        int yMax = 0;
+        int vMax = 0;
+        int sMax = 0;
+
+        for (int y = 0; y < 300; ++y)
+        {
+            for (int x = 0; x < 300; ++x)
+            {
+                int value = 0;
+                int maxSize = min(300 - x, 300 - y);
+
+                for (int size = 0; size < maxSize; ++size)
+                {
+                    for (int iy = 0; iy < size; ++iy)
+                    {
+                        value += cells[y + iy][x + size];
+                    }
+                    for (int ix = 0; ix < size; ++ix)
+                    {
+                        value += cells[y + size][x + ix];
+                    }
+                    value += cells[y + size][x + size];
+
+                    if (value > vMax)
+                    {
+                        xMax = x + 1;
+                        yMax = y + 1;
+                        vMax = value;
+                        sMax = size + 1;
+                    }
+                }
+            }
+        }
+
+        printf("Max power of %d starting at (%d, %d) with size %dx%d\n", vMax, xMax, yMax, sMax, sMax);
+    }
 
     std::getc(stdin);
 }
